89354e57ca23d697cc671ba9fb1b409ef30c74b9
Po-Yu Yang <corupted5354.cv99@g2.nctu.edu.tw>
2019-05-22 01:46:13 -0400

    Users and Posts functions

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {createPost, getPosts} = require('../controllers/post');
+const {createPost, getPosts, postsByUser, postById, isPoster, deletePost, updatePost } = require('../controllers/post');
 const { requireSignin } = require('../controllers/auth');
 const { createPostValidaotr } = require('../validators');
 const { userById } = require('../controllers/user');
@@ -7,12 +7,22 @@ const { userById } = require('../controllers/user');
 const router = express.Router();
 // Dont need to specify index
 
-router.get('/', requireSignin, getPosts);
-router.post('/post', requireSignin, createPostValidaotr, createPost);
+router.get("/posts", getPosts);
+router.post(
+    '/post/new/:userId', 
+    requireSignin, 
+    createPost,
+    createPostValidaotr
+);
+router.get("/posts/by/:userId", requireSignin, postsByUser);
+router.delete("/post/:postId", requireSignin, isPoster, deletePost);
+router.put("/post/:postId", requireSignin, isPoster, updatePost);
 
 
 // any route containing :userId, app will first execute userByID()
 router.param('userId', userById);
+// any route containing :postById, app will first execute postById()
+router.param('postId', postById);
 
 
 
