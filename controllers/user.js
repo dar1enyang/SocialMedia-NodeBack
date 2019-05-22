a63c49111b653769e7105aefa1ec45b5fbe38ee9
Po-Yu Yang <corupted5354.cv99@g2.nctu.edu.tw>
2019-05-21 21:22:15 -0400

    added user related features - showing all users, single user, update user, delete user

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-
+const _ = require('lodash');
 
 exports.userById = (req, res, next, id) => {
     User.findById(id).exec((err, user) => {
@@ -21,3 +21,53 @@ exports.hasAuthorization = (req, res, next) => {
         });
     }
 };
+
+exports.allUsers = (req, res) => {
+    User.find((err, users) => {
+        if (err) {
+            return res.status(400).json({
+                error: err
+            })
+        }
+        res.json({ users });
+    }).select('name email updated created');
+};
+
+exports.getUser = (req, res) => {
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    return res.json(req.profile);
+};
+
+exports.updateUser = (req, res, next) => {
+    let user = req.profile;
+    user = _.extend(user, req.body); // extend - mutate the source object
+    user.updated = Date.now();
+    user.save(err => {
+        if (err) {
+            return res.status(400).json({
+                error: "You are not authorized to perform this action"
+            });
+        }
+        user.hashed_password = undefined;
+        user.salt = undefined;
+        res.json({ user });
+    });
+};
+
+exports.deleteUser = (req, res, next) => {
+    let user = req.profile;
+    user.remove((err, user) => {
+        if (err) {
+            return res.status(400).json({
+                error: err
+            });
+        }
+        user.hashed_password = undefined;
+        user.salt = undefined;
+        res.json({ user });
+    });
+};
+
+
+
