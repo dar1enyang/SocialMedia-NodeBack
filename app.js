071ddae9ec52350c7db6324d36647d8607788fab
Po-Yu Yang <corupted5354.cv99@g2.nctu.edu.tw>
2019-05-26 23:42:08 -0400

    change user controller

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 // import mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // load env variables
-const dotenv = require('dotenv');
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
-const expressValidator = require('express-validator')
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
 const fs = require("fs");
-const cors = require('cors');
-
+const cors = require("cors");
 
 // bring in routes
-const postRoutes = require('./routes/post');
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
+const postRoutes = require("./routes/post");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 
 // middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
@@ -27,37 +26,39 @@ app.use(expressValidator());
 app.use("/", postRoutes);
 app.use("/", authRoutes);
 app.use("/", userRoutes);
-app.use(function (err, req, res, next) {
-  if (err.name === 'UnauthorizedError') {
-    res.status(401).json({ error: "Unauthorized!" });
+app.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({
+      error: "Unauthorized!",
+    });
   }
 });
 // apiDocs
 app.get("/", (req, res) => {
-    fs.readFile("docs/APIDocs.json", (err, data) => {
-        if (err) {
-            res.status(400).json({
-                error: err
-            });
-        }
-        const docs = JSON.parse(data);
-        res.json(docs);
-    });
+  fs.readFile("docs/APIDocs.json", (err, data) => {
+    if (err) {
+      res.status(400).json({
+        error: err,
+      });
+    }
+    const docs = JSON.parse(data);
+    res.json(docs);
+  });
 });
 
 //db connection
-mongoose.connect(
-  process.env.MONGO_URI,
-  {useNewUrlParser: true}
-)
-.then(() => console.log('DB Connected'))
- 
-mongoose.connection.on('error', err => {
-  console.log(`DB connection error: ${err.message}`)
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", err => {
+  console.log(`DB connection error: ${err.message}`);
 });
 
 //
 const port = 8080;
 app.listen(port, () => {
-    console.log(`A Node Js API is listening on port: ${port}`)
+  console.log(`A Node Js API is listening on port: ${port}`);
 });
--- models/user.js
+++ models/user.js
@@ -1,47 +1,47 @@
-const mongoose = require("mongoose");
-const uuidv1 = require("uuid/v1");
-const crypto = require("crypto");
+const mongoose = require('mongoose');
+const uuidv1 = require('uuid/v1');
+const crypto = require('crypto');
 const { ObjectId } = mongoose.Schema;
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    email: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    hashed_password: {
-        type: String,
-        required: true
-    },
-    salt: String,
-    created: {
-        type: Date,
-        default: Date.now
-    },
-    updated: Date,
-    photo: {
-        data: Buffer,
-        contentType: String
-    },
-    about: {
-        type: String,
-        trim: true
-    },
-    following: [{ type: ObjectId, ref: "User" }],
-    followers: [{ type: ObjectId, ref: "User" }],
-    resetPasswordLink: {
-        data: String,
-        default: ""
-    },
-    role: {
-        type: String,
-        default: "subscriber"
-    }
+  name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  hashed_password: {
+    type: String,
+    required: true
+  },
+  salt: String,
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  updated: Date,
+  photo: {
+    data: Buffer,
+    contentType: String
+  },
+  about: {
+    type: String,
+    trim: true
+  },
+  following: [{ type: ObjectId, ref: 'User' }],
+  followers: [{ type: ObjectId, ref: 'User' }],
+  resetPasswordLink: {
+    data: String,
+    default: ''
+  },
+  role: {
+    type: String,
+    default: 'subscriber'
+  }
 });
 
 /**
@@ -53,36 +53,36 @@ const userSchema = new mongoose.Schema({
 
 // virtual field
 userSchema
-    .virtual("password")
-    .set(function(password) {
-        // create temporary variable called _password
-        this._password = password;
-        // generate a timestamp
-        this.salt = uuidv1();
-        // encryptPassword()
-        this.hashed_password = this.encryptPassword(password);
-    })
-    .get(function() {
-        return this._password;
-    });
+  .virtual('password')
+  .set(function(password) {
+    // create temporary variable called _password
+    this._password = password;
+    // generate a timestamp
+    this.salt = uuidv1();
+    // encryptPassword()
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
 
 // methods
 userSchema.methods = {
-    authenticate: function(plainText) {
-        return this.encryptPassword(plainText) === this.hashed_password;
-    },
+  authenticate: function(plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
 
-    encryptPassword: function(password) {
-        if (!password) return "";
-        try {
-            return crypto
-                .createHmac("sha1", this.salt)
-                .update(password)
-                .digest("hex");
-        } catch (err) {
-            return "";
-        }
+  encryptPassword: function(password) {
+    if (!password) return '';
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex');
+    } catch (err) {
+      return '';
     }
+  }
 };
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
