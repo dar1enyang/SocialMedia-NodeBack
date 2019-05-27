e2db037ee3ba6f51807a5cafce07f45820ce02c1
Po-Yu Yang <corupted5354.cv99@g2.nctu.edu.tw>
2019-05-27 07:14:26 -0400

    Password forgot and reset

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const { signup, signin, signout } = require('../controllers/auth');
+const {
+  signup,
+  signin,
+  signout,
+  forgotPassword,
+  resetPassword
+} = require('../controllers/auth');
 const { userById } = require('../controllers/user');
-const { userSignupValidator } = require('../validators');
-
+const {
+  userSignupValidator,
+  passwordResetValidator
+} = require('../validators');
 
 const router = express.Router();
 
@@ -10,9 +18,11 @@ router.post('/signup', userSignupValidator, signup);
 router.post('/signin', signin);
 router.get('/signout', signout);
 
+// password forgot and reset routes
+router.put('/forgot-password', forgotPassword);
+router.put('/reset-password', passwordResetValidator, resetPassword);
+
 // any route containing :userId, app will first execute userByID()
 router.param('userId', userById);
-
-
 
 module.exports = router;
