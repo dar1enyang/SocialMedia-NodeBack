c2626b6fe9a84d0bbea67f0e2c3f324fe17a942c
Po-Yu Yang <corupted5354.cv99@g2.nctu.edu.tw>
2019-05-28 16:56:01 -0400

    typo error

--- routes/post.js
+++ routes/post.js
@@ -15,11 +15,11 @@ const {
   uncomment
 } = require('../controllers/post');
 const { requireSignin } = require('../controllers/auth');
-const { createPostValidaotr } = require('../validators');
+const { createPostValidator } = require('../validators');
 const { userById } = require('../controllers/user');
 
 const router = express.Router();
-// Dont need to specify index
+// Don't need to specify index
 
 router.get('/posts', getPosts);
 
@@ -35,7 +35,7 @@ router.post(
   '/post/new/:userId',
   requireSignin,
   createPost,
-  createPostValidaotr
+  createPostValidator
 );
 router.get('/posts/by/:userId', requireSignin, postsByUser);
 router.get('/post/:postId', singlePost);
